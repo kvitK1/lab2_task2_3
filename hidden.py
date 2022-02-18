930fa9c7e76986c988c9400a29750f906ea6ff27
kvitK1 <kkvts11@gmail.com>
2022-02-18 19:45:31 +0200

    oops

--- hidden.py
+++ hidden.py
@@ -4,7 +4,7 @@
 # Create new App and get the four strings
 
 def oauth():
-    return {"consumer_key": "0PlKlKMbNYWL9UeVuC6DPxX4B",
-            "consumer_secret": "dpvpCGjA9udpjsPkCVfdxZhmAOLMXFeZcZEVLbH3usRiv8fHfN",
-            "token_key": "963011359737774080-D73fzqJ2jocOftSRejq7tJz2gn4Xa2n",
-            "token_secret": "KJ9zSSyrJsYLidt7PQTyPeAqZvrB92bxWwlzv7Ma8hMcG"}
+    return {"consumer_key": "<input your one>",
+            "consumer_secret": "<input your one>",
+            "token_key": "<input your one>",
+            "token_secret": "<input your one>"}
